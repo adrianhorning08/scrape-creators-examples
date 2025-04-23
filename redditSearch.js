cae4ee5167be98e0d9e98403b82b6be9a53ecc6d
Adrian Horning <m.adrian.horning@gmail.com>
2025-04-23 12:36:37 -0500

    update

--- redditSearch.js
+++ redditSearch.js
@@ -3,9 +3,9 @@ import fs from "graceful-fs";
 
 (async () => {
   const start = new Date();
-  const NUM_RESULTS = 100;
+  const NUM_RESULTS = 20;
   const term = "tariffs";
-  const sort = "comment_count"; // relevance, new, top, comment_count
+  const sort = "relevance"; // relevance, new, top, comment_count
   const timeframe = "all"; // all, day, week, month, year
   let after = null;
   const results = [];
@@ -21,7 +21,12 @@ import fs from "graceful-fs";
     }
   }
 
-  console.log(results);
+  results.forEach((result) => {
+    console.log(result.title);
+    console.log(result.url);
+    console.log("--------------------------------");
+  });
+
   console.log(results.length);
   fs.writeFileSync("test.json", JSON.stringify(results, null, 2));
   const end = new Date();
